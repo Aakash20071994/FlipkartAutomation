1a6d20e971883b0878177dd1319822e11bdefb56
unknown <kaduaakash@gmail.com>
2024-10-14 17:21:35 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,38 +1,43 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.internal.EclipseInterface;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 
-
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import dev.failsafe.internal.util.Durations;
 
 public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,16 +51,107 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         driver.manage().window().maximize();
     }
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Start of TestCase01");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        Wrappers.Url(driver, "https://www.flipkart.com/");
+        Wrappers.search(driver, "Washing Machine");
+        WebElement popularityTab = wait
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[text()='Popularity']")));
+        popularityTab.click();
+        Thread.sleep(4000);
+        List<WebElement> ratings = driver.findElements(By.xpath("//span[@class='Y1HWO0']//div[@class='XQDdHH']"));
+        Thread.sleep(4000);
+        int count = 0;
+        for (int i = 0; i < ratings.size(); i++) {
+            String text = ratings.get(i).getText();
+            Thread.sleep(2000);
+            double rating = Double.parseDouble(text);
+            if (rating <= 4.0) {
+                count = count + 1;
+            }
+
+        }
+        System.out.println("Count Less than or equlas 4:" + count);
+
+        System.out.println("TestCase01:Pass");
+        System.out.println("End of TestCase");
+
+    }
+
+   @Test
+    public void testCase02() throws InterruptedException {
+        System.out.println("Start of TestCase02");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        Wrappers.Url(driver, "https://www.flipkart.com/");
+        try {
+            WebElement popupCloseButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='_30XB9F']")));
+            
+            if (popupCloseButton.isDisplayed()) {
+                popupCloseButton.click();  // Close the pop-up
+                System.out.println("Login pop-up closed.");
+            }
+        } catch (Exception e) {
+            System.out.println("Login pop-up not present, continuing with the test.");
+        }
+        Wrappers.search(driver, "iPhone");
+        Thread.sleep(4000);
+        List<WebElement> listOfitems = wait
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='KzDlHZ']")));
+        List<WebElement> discount = wait
+                .until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("//div[@class='UkUFwK']")));
+        int itemCount = Math.min(listOfitems.size(), discount.size());
+
+        for (int i = 0; i < itemCount; i++) {
+            String listOfItemText = listOfitems.get(i).getText();
+            String TextofDiscount = discount.get(i).getText();
+            String discountNum = TextofDiscount.replaceAll("[^0-9.]", "");
+            int discontElement = Integer.parseInt(discountNum);
+            if (discontElement >= 17) {
+                System.out.println("Title:" + listOfItemText + " " + "Discount:" + discontElement + "%");
+            }
+
+        }
+        System.out.println("TestCase02:Pass");
+        System.out.println("End of TestCase");
+
+    }
+    @Test
+    public void testCase03() throws InterruptedException{
+        System.out.println("Start of TestCase03");
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
+        Wrappers.Url(driver, "https://www.flipkart.com/");
+
+        try {
+            WebElement popupCloseButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@class='_30XB9F']")));
+            
+            if (popupCloseButton.isDisplayed()) {
+                popupCloseButton.click();  // Close the pop-up
+                System.out.println("Login pop-up closed.");
+            }
+        } catch (Exception e) {
+            System.out.println("Login pop-up not present, continuing with the test.");
+        }
+        Wrappers.search(driver, "Coffee Mug");
+        Thread.sleep(2000);
+        Wrappers.sortByRating(driver, "4");
+        Thread.sleep(5000);
+        Wrappers.review(driver);
+        System.out.println("TestCase03:Pass");
+        System.out.println("End of TestCase");
+       
+    }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
@@ -19,4 +20,57 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    public static void Url(WebDriver driver, String url) {
+        driver.get(url);
+    }
+
+    public static void search(WebDriver driver, String keys) throws InterruptedException {
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        searchBox.clear();
+        searchBox.sendKeys(keys);
+        Thread.sleep(4000);
+        searchBox.submit();
+        
+     
+
+    }
+
+    public static void sortByRating(WebDriver driver, String rating) {
+        WebElement rate = driver.findElement(
+                By.xpath("//div[contains(text(),'" + rating + "') ]/preceding-sibling::div[@class='XqNaEv']"));
+        rate.click();
+    }
+
+    public static void review(WebDriver driver) {
+        List<WebElement> titles = driver.findElements(By.xpath("//a[@class='wjcEIp']"));
+        List<WebElement> image = driver.findElements(By.xpath("//a[@class='VJA3rP']//img"));
+        List<WebElement> reviewCounts = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+        List<Integer> countList = new ArrayList<>();
+        List<Integer> index = new ArrayList<>();
+        for (int i = 0; i < reviewCounts.size(); i++) {
+            String text = reviewCounts.get(i).getText().replaceAll("[,()]", "");
+            int count = Integer.parseInt(text);
+            countList.add(count);
+            index.add(i);
+
+        }
+        for (int i = 0; i < index.size() - 1; i++) {
+            for (int j = 0; j < index.size() - i - 1; j++) {
+                if (countList.get(index.get(j)) < countList.get(index.get(j + 1))) {
+                    // Swap the indices
+                    int temp = index.get(j);
+                    index.set(j, index.get(j + 1));
+                    index.set(j + 1, temp);
+                }
+            }
+        }
+        for (int i = 0; i < 5 && i < index.size(); i++) {
+            int ind = index.get(i);
+            System.out.println("Title of Element:" + titles.get(ind).getText());
+            System.out.println("Image Link:" + image.get(ind).getAttribute("src"));
+            System.out.println("Review count:" + countList.get(ind));
+
+        }
+    }
+    
 }
